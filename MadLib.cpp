e93cfc92299cc739badf2e246bda87c9df6cbd8c
strubium <113206902+strubium@users.noreply.github.com>
2024-04-22 12:00:21 -0500

    Document code

--- MadLib.cpp
+++ MadLib.cpp
@@ -7,31 +7,36 @@ using namespace std;
 
 
 class MadLib {
-    string MadLib::randomAmount() {
-        random_device rd;
-        mt19937 gen(rd());
+/**
+ * @brief Returns a random amount adverb
+ * @details This function generates a random number between 0 and 3, rounds it to the nearest integer, and returns one of the four adverbs "really", "super", "mega", or "ultra" based on the resulting number. If the random number is outside the range of 0 and 3, the adverb "mega" is returned.
+ * @return A random adverb
+ */
+string MadLib::randomAmount() {
+    random_device rd;
+    mt19937 gen(rd());
 
-        uniform_real_distribution<> dis(0.0, 3.0);
+    uniform_real_distribution<> dis(0.0, 3.0);
 
-        double randomNumber = dis(gen);
+    double randomNumber = dis(gen);
 
-        // Round the number
-        int roundedNumber = round(randomNumber);
-        switch (roundedNumber) {
-        case 1:
-            return "really";
-            break;
-        case 2:
-            return "super";
-            break;
-        case 3:
-            return "mega";
-            break;
-        case 4:
-            return "ultra";
-            break;
-        default:
-            return "mega";
-        }
+    // Round the number
+    int roundedNumber = round(randomNumber);
+    switch (roundedNumber) {
+    case 0:
+        return "really";
+        break;
+    case 1:
+        return "super";
+        break;
+    case 2:
+        return "mega";
+        break;
+    case 3:
+        return "ultra";
+        break;
+    default:
+        return "mega";
     }
+}
 };
--- PlayerCharacter.cpp
+++ PlayerCharacter.cpp
@@ -1,20 +1,42 @@
 #include "PlayerCharacter.h"
 
+/**
+ * @brief Sets the player's name
+ * 
+ * @param inputName The player's name
+ * @return std::string The player's name
+ */
 std::string PlayerCharacter::setPlayerName(std::string inputName) {
     playerName = inputName;
     return playerName; // Return the assigned name
 }
 
+/**
+ * @brief Returns the player's name
+ * 
+ * @return std::string The player's name
+ */
 std::string PlayerCharacter::getPlayerName() {
     return playerName; // Return the assigned name
 }
 
 
+/**
+ * @brief Sets the player's gender
+ * 
+ * @param inputGender The player's gender
+ * @return true if the player is male, false if the player is female
+ */
 bool PlayerCharacter::setGender(bool inputGender) {
     playerGender = inputGender;
     return playerGender; // Return the assigned gender
 }
 
+/**
+ * @brief Returns the player's gender
+ * 
+ * @return true if the player is male, false if the player is female
+ */
 bool PlayerCharacter::getGender() {
     return playerGender;
 }
